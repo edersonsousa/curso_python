60f3b892ea238d7e7f372f3a4af9353e4d6db086
edersonsousa <ederson.sousadasilva@gmail.com>
2024-01-03 17:05:10 -0300

    Iterando Strings com o While| Com a devida correção

--- aula39.py
+++ aula39.py
@@ -3,14 +3,23 @@
 
 
 """
-import time
 nome = 'Ederson Sousa' # Iteráveis
 
 tamanho = len(nome)
 i = 0
 
-while i <= tamanho:
-    time.sleep(1)  # import time
+while i < tamanho:
     print(nome[i])
     i += 1
+print('|')
+nome2 = 'Ederson Sousa'  # Iteráveis
 
+indice = 0
+novo_nome = ''
+while indice < len(nome2):
+    letra = nome2[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)
