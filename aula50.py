797423907d1f1365535f7c6612338135b3583b24
edersonsousa <ederson.sousadasilva@gmail.com>
2024-01-27 17:08:58 -0300

    Exercício exiba os índices da lista resolução do professor

--- aula50.py
+++ aula50.py
@@ -1,16 +1,15 @@
 """
-
-Exercício exiba os índices da lista
-
-
-
+Exercício
+Exiba os índices da lista
+0 Maria
+1 Helena
+2 Luiz
 """
-
 lista = ['Maria', 'Helena', 'Luiz']
-i = 0
-for nome in lista:
-    
-    print(i , nome)
-    i += 1
+lista.append('João')
 
-#print (lista)
+
+indices = range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice], type(lista[indice]))
