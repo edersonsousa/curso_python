e339b8b1aaad76e9e811f56e1260b78e4c66d3f4
edersonsousa <ederson.sousadasilva@gmail.com>
2024-01-27 16:34:26 -0300

    Concatenando e estendendo listas

--- aula48.py
+++ aula48.py
@@ -45,3 +45,25 @@ lista.append(60)
 lista.append(70)
 ultimo_valor = lista.pop(3)
 print(lista, 'Removido,', ultimo_valor)
+
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append - Adiciona um item ao final
+    insert - Adiciona um item no índice escolhido
+    pop - Remove do final ou do índice escolhido
+    del - apaga um índice
+    clear - limpa a lista
+    extend - estende a lista
+    + - concatena listas
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+lista_c = lista_a + lista_b
+lista_a.extend(lista_b)
+print(lista_a)
